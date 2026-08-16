1c0104c77fcbe2a7b528adb01835aa5cd408f3fc
zeeshansq <zeeshan.shabbirqureshi@gmail.com>
2026-08-16 16:01:16 +0500

    feat: implement CI/CD pipeline, add stylized login template, and update testing configuration.

--- conftest.py
+++ conftest.py
@@ -8,3 +8,4 @@ from django.conf import settings
 def pytest_configure(config):
     import os
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cafems.settings.development")
+    os.environ.setdefault("SECRET_KEY", "django-insecure-ci-testing-key-cafems-test-suite-2026")
